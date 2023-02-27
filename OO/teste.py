aacc795b44080c3a8edec767da088afa94be60b2
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-02-27 18:34:52 -0300

    Update teste.py

--- OO/teste.py
+++ OO/teste.py
@@ -1,3 +1,16 @@
 def cria_conta(numero, titular, saldo, limite):
-    conta = {"Número": numero, "Titular": titular, "Saldo": saldo, "limite": limite}
+    conta = {"número": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
+
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+
+def extrato(conta):
+    print(f"O saldo é {conta["Saldo"]}")
+
