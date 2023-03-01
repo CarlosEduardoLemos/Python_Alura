7f758a0963d1c89f12ab4606a99c4b3f19b3596f
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-03-01 16:54:18 -0300

    Update conta.py

--- OO/conta.py
+++ OO/conta.py
@@ -1,4 +1,16 @@
 class ContaCorrente:
-    def __init__(self):
+    def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo objeto ... {self}")
-        
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print(f"Saldo {self.saldo} do titular {self.titular}")
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saca -= valor
