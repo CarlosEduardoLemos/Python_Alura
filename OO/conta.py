6ee2653750351ad0ea666231f4e479911711fd10
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-03-03 11:29:09 -0300

    Update conta.py

--- OO/conta.py
+++ OO/conta.py
@@ -6,11 +6,19 @@ class ContaCorrente:
         self.__saldo = saldo
         self.__limite = limite
 
+
     def extrato(self):
         print(f"Saldo {self.__saldo} do titular {self.__titular}")
+
 
     def deposita(self, valor):
         self.__saldo += valor
 
+
     def saca(self, valor):
         self.__saldo -= valor
+
+
+    def trasnfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
