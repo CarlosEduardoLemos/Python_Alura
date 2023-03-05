6497844cdb227f4704750d2d336358137167eedb
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-03-05 11:32:18 -0300

    Update conta.py

--- OO/conta.py
+++ OO/conta.py
@@ -4,30 +4,37 @@ class Programa:
         self.ano = ano
         self._likes = 0
 
+
     @property
     def likes(self):
         return self._likes
 
+
     def dar_likes(self):
         self._likes += 1
+
 
     @property
     def nome(self):
         return self._nome
 
+
     @nome.setter
     def nome(self, nome):
         self._nome = nome
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
 
 vingadores = Filme('vingadores: guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
