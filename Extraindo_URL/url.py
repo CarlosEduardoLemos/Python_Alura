2511c215cd429c8b383ff123f65e069c8b9b7962
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-03-08 16:26:44 -0300

    Update url.py

--- Extraindo_URL/url.py
+++ Extraindo_URL/url.py
@@ -1,9 +1,17 @@
 url = "https://bytebank.com/cambio?moedaOrigem=real"
 print(url)
 
-indice_interrompe = url.find("?")
-url_base = url[:indice_interrompe]
+indice_interrogacao = url.find("?")
+url_base = url[:indice_interrogacao]
 print(url_base)
 
-url_parametros = url[indice_interrompe+1:]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+paremetro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(paremetro_busca)
+
+indice_valor = indice_parametro + len(paremetro_busca) +1
+valor = url_parametros[indice_valor:]
+
+print(valor)
