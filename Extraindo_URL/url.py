3593ae6c74b2ff5fcaaa39d4f0871e597e9b8f38
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-03-08 17:05:40 -0300

    Update url.py

--- Extraindo_URL/url.py
+++ Extraindo_URL/url.py
@@ -1,17 +1,40 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real"
-print(url)
+class ExtratorURL:
+    def __init__(self, url):
+        self.url = self.sanitiza_url(url)
+        self.valida_url()
 
-indice_interrogacao = url.find("?")
-url_base = url[:indice_interrogacao]
-print(url_base)
+    def sanitiza_url(self, url):
+        if type(url) == str:
+            return url.strip()
+        else:
+            return ""
 
-url_parametros = url[indice_interrogacao+1:]
-print(url_parametros)
+    def valida_url(self):
+        if not self.url:
+            raise ValueError("A URL está vazia")
 
-paremetro_busca = 'moedaOrigem'
-indice_parametro = url_parametros.find(paremetro_busca)
+    def get_url_base(self):
+        indice_interrogacao = self.url.find('?')
+        url_base = self.url[:indice_interrogacao]
+        return url_base
 
-indice_valor = indice_parametro + len(paremetro_busca) +1
-valor = url_parametros[indice_valor:]
+    def get_url_parametros(self):
+        indice_interrogacao = self.url.find('?')
+        url_parametros = self.url[indice_interrogacao + 1:]
+        return url_parametros
 
-print(valor)
+    def get_valor_parametro(self, parametro_busca):
+        indice_parametro = self.get_url_parametros().find(parametro_busca)
+        indice_valor = indice_parametro + len(parametro_busca) + 1
+        indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
+        if indice_e_comercial == -1:
+            valor = self.get_url_parametros()[indice_valor:]
+        else:
+            valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
+        return valor
+
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+valor_quantidade = extrator_url.get_valor_parametro("quantidade")
+print(valor_quantidade)
