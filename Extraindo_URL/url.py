5132a8d4403d1ac49f7d02881708b71ca2daf01e
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-03-08 18:01:27 -0300

    Update url.py

--- Extraindo_URL/url.py
+++ Extraindo_URL/url.py
@@ -1,7 +1,9 @@
+import re
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
         self.valida_url()
+
 
     def sanitiza_url(self, url):
         if type(url) == str:
@@ -9,19 +11,27 @@ class ExtratorURL:
         else:
             return ""
 
+
     def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
+        padrao_url = re.compile('(http(s)?://)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(url)
+        if not match:
+            raise ValueError("A URL está válida")
+
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
         url_base = self.url[:indice_interrogacao]
         return url_base
 
+
     def get_url_parametros(self):
         indice_interrogacao = self.url.find('?')
         url_parametros = self.url[indice_interrogacao + 1:]
         return url_parametros
+
 
     def get_valor_parametro(self, parametro_busca):
         indice_parametro = self.get_url_parametros().find(parametro_busca)
